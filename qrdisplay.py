2941ee8f70e71d66e75f9d4edd3563d0c28d9fad
Wilfried van Asten <wilfriedvanasten@gmail.com>
2018-08-21 14:20:56 +0200

    Rewrite to use events
    Added the potential to run as a daemon.

--- qrdisplay.py
+++ qrdisplay.py
@@ -1,134 +1,70 @@
 #!/usr/bin/env python
 
-##
- #  @filename   :   main.cpp
- #  @brief      :   2.9inch e-paper display (B) demo
- #  @author     :   Yehui from Waveshare
- #
- #  Copyright (C) Waveshare     July 31 2017
- #
- # Permission is hereby granted, free of charge, to any person obtaining a copy
- # of this software and associated documnetation files (the "Software"), to deal
- # in the Software without restriction, including without limitation the rights
- # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- # copies of the Software, and to permit persons to  whom the Software is
- # furished to do so, subject to the following conditions:
- #
- # The above copyright notice and this permission notice shall be included in
- # all copies or substantial portions of the Software.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- # THE SOFTWARE.
- ##
-
 import time
 import sys
-import epd2in7b
+import getpass
+import os.path as path
 import RPi.GPIO as GPIO
-import qrcode
-import Image
-import ImageFont
-import ImageDraw
-import textwrap
-#import imagedata
+from xdg.BaseDirectory import (save_config_path)
+import configparser
+import wifi
 
-COLORED = 1
-UNCOLORED = 0
-
-def generate_qr_image(ssid, key):
-    qr = qrcode.QRCode(
-        version=2,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=5,
-        border=2
-    )
-    qr.add_data('WIFI:S:{};T:WPA;P:{};;'.format(ssid, key))
-    qr.make()
-    return qr.make_image(fill_color="black", back_color="white")
-
-
-def display_qr_code(ssid, key):
-    epd = epd2in7b.EPD()
-    epd.init()
-    epd.set_rotate(epd2in7b.ROTATE_0)
-
-    # clear the frame buffer
-    frame_black = [0] * (epd.width * epd.height / 8)
-    frame_red = [0] * (epd.width * epd.height / 8)
-
-    qr_image = generate_qr_image(ssid, key)
-    width, height = qr_image.size
-    image_padding = max(0, (epd.width - width) // 2)
-    epd.draw_image(
-            frame_black,
-            max(0, (epd.width - width) // 2),
-            max(0, (epd.height - height) // 2),
-            qr_image
-        )
-
-    epd.display_frame(frame_black, frame_red)
-
-    # You can get frame buffer from an image or import the buffer directly:
-    #epd.display_frame(imagedata.IMAGE_BLACK, imagedata.IMAGE_RED)
-    epd.sleep()
-    GPIO.cleanup()
-
-def display_ssid_and_key(ssid, key):
-    epd = epd2in7b.EPD()
-    epd.init()
-    epd.set_rotate(epd2in7b.ROTATE_90)
-    
-    # clear the frame buffer
-    frame_black = [0] * (epd.width * epd.height / 8)
-    frame_red = [0] * (epd.width * epd.height / 8)
-    
-    font_height = 16
-    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', font_height)
-    line_height = font_height + 2
-    y = 2
-    epd.draw_string_at(frame_black, 2, y, "SSID:", font, COLORED)
-    y = y + line_height
-    epd.draw_string_at(frame_black, 2, y,"{}".format(ssid), font, COLORED)
-    y = y + line_height
-    epd.draw_string_at(frame_black, 2, y, "Key:", font, COLORED)
-    y = y + line_height
-    epd.draw_string_at(frame_black, 2, y, "{}".format(" ".join(textwrap.wrap(key, 4))), font, COLORED)
-
-    epd.display_frame(frame_black, frame_red)
-
-    # You can get frame buffer from an image or import the buffer directly:
-    #epd.display_frame(imagedata.IMAGE_BLACK, imagedata.IMAGE_RED)
-    epd.sleep()
-    GPIO.cleanup()
 
 def wait_for_button_press(t=1000):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     c = GPIO.wait_for_edge(5, GPIO.FALLING, timeout=t)
-    GPIO.cleanup()
     return c
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: qrdisplay ssid key")
+    if len(sys.argv) > 2:
+        print("Usage: qrdisplay [-d]")
         sys.exit(1)
-    ssid = sys.argv[1]
-    key = sys.argv[2]
-    while True:
-        try:
-            display_qr_code(ssid, key)
-            while wait_for_button_press() is None:
-                pass
-            display_ssid_and_key(ssid, key)
-            time.sleep(30)
-        except KeyboardInterrupt:
-            GPIO.cleanup()
-            raise
+    run_as_daemon = False
+    if len(sys.argv) == 2:
+        if sys.argv[1] == '-d':
+            runAsDaemon = True
+        else:
+            print("Usage: qrdisplay [-d]")
+            sys.exit(1)
+    config = configparser.ConfigParser()
+
+    config_path = save_config_path("qrdisplay")
+    config_filename = path.join(config_path, "config")
+
+    if not path.isfile(config_filename):
+        if run_as_daemon:
+            print("No config file exists yet. First run the program in the front so one can be created or create a config file at {}", config_file)
+            sys.exit(1)
+        else:
+            print("Creating the config file")
+            ssid = raw_input("SSID: ")
+            key = getpass.getpass("Key: ")
+            moonlight_host = raw_input("Moonlight Host: ")
+            moonlight_arguments = raw_input("Moonlight Stream Arguments: ")
+            config['WIFI'] = {
+                    'ssid': ssid,
+                    'key': key
+                }
+            config['Moonlight'] = {
+                    'host': moonlight_host,
+                    'arguments': moonlight_arguments
+                }
+            with open(config_filename, 'w') as config_file:
+                config.write(config_file)
+            sys.exit(0)
+    else:
+        config.read(config_filename)
+        ssid = config['WIFI']['ssid']
+        key = config['WIFI']['key']
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        wifi.display_qr_code(ssid, key)
+        GPIO.add_event_detect(5, GPIO.FALLING, callback=wifi.create_event_callback(ssid, key))
+        while True:
+           try:
+               time.sleep(30)
+           except KeyboardInterrupt:
+               GPIO.cleanup()
+               raise
         
 
 if __name__ == '__main__':
---
+++ wifi.py
@@ -0,0 +1,90 @@
+import time
+import epd2in7b
+import RPi.GPIO as GPIO
+import qrcode
+import Image
+import ImageFont
+import ImageDraw
+import textwrap
+import threading
+
+COLORED = 1
+UNCOLORED = 0
+
+def generate_qr_image(ssid, key):
+    qr = qrcode.QRCode(
+        version=2,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=5,
+        border=2
+    )
+    qr.add_data('WIFI:S:{};T:WPA;P:{};;'.format(ssid, key))
+    qr.make()
+    return qr.make_image(fill_color="black", back_color="white")
+
+
+def display_qr_code(ssid, key):
+    epd = epd2in7b.EPD()
+    epd.init()
+    epd.set_rotate(epd2in7b.ROTATE_0)
+    
+    frame_black = [0] * (epd.width * epd.height / 8)
+    frame_red = [0] * (epd.width * epd.height / 8)
+
+    qr_image = generate_qr_image(ssid, key)
+    width, height = qr_image.size
+    image_padding = max(0, (epd.width - width) // 2)
+    epd.draw_image(
+            frame_black,
+            max(0, (epd.width - width) // 2),
+            max(0, (epd.height - height) // 2),
+            qr_image
+        )
+
+    epd.display_frame(frame_black, frame_red)
+    epd.sleep()
+
+def display_ssid_and_key(ssid, key):
+    epd = epd2in7b.EPD()
+    epd.init()
+    epd.set_rotate(epd2in7b.ROTATE_90)
+    
+    frame_black = [0] * (epd.width * epd.height / 8)
+    frame_red = [0] * (epd.width * epd.height / 8)
+    
+    font_height = 16
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', font_height)
+    line_height = font_height + 2
+    y = 2
+    epd.draw_string_at(frame_black, 2, y, "SSID:", font, COLORED)
+    y = y + line_height
+    epd.draw_string_at(frame_black, 2, y,"{}".format(ssid), font, COLORED)
+    y = y + line_height
+    epd.draw_string_at(frame_black, 2, y, "Key:", font, COLORED)
+    y = y + line_height
+    epd.draw_string_at(frame_black, 2, y, "{}".format(" ".join(textwrap.wrap(key, 4))), font, COLORED)
+
+    epd.display_frame(frame_black, frame_red)
+
+    # You can get frame buffer from an image or import the buffer directly:
+    #epd.display_frame(imagedata.IMAGE_BLACK, imagedata.IMAGE_RED)
+    epd.sleep()
+
+def create_toggle_code(ssid, key):
+    display_ssid_and_key(ssid, key)
+    time.sleep(30)
+    display_qr_code(ssid, key)
+
+
+display_toggle_thread = None
+
+def create_event_callback(ssid, key):
+    def event_callback(channel):
+        global display_toggle_thread
+        if display_toggle_thread is None or not display_toggle_thread.is_alive():
+            print("Handling button {} press".format(channel))
+            display_toggle_thread = threading.Thread(target = create_toggle_code, name = "Display toggle", args=(ssid, key))
+            display_toggle_thread.start()
+        else:
+            print("Already handling {} button press".format(channel))
+    return event_callback
